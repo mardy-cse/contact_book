95d431bf03cb7ec08ad6ea75144e605479d5527b
John Liton Mardy <mardy.cse@gmail.com>
2024-08-17 01:27:59 +0600

    save and get contacts

--- lib/features/contacts/presentation/controller/contact_controller.dart
+++ lib/features/contacts/presentation/controller/contact_controller.dart
@@ -1,9 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ContactController extends GetxController {
   RxBool isGridView = false.obs;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
 
   void toggleView() {
     isGridView.value = !isGridView.value;
+  }
+
+  Future saveContact({
+    required String name,
+    required String number,
+    String? email,
+    String? address,
+  }) async {
+    await FirebaseFirestore.instance.collection('Contacts').doc(name).set({
+      'name': nameController.text.toString(),
+      'number': numberController.text.toString(),
+      'email': emailController.text.toString(),
+      'address': addressController.text.toString()
+    }).then(
+      (value) {
+        nameController.clear();
+        numberController.clear();
+        emailController.clear();
+        addressController.clear();
+      },
+    );
   }
 }
